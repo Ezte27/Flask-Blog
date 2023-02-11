d5409c75d849a92afbb2134bf99f4753e2e56923
Esteban Calderon <estedcg27@gmail.com>
2023-02-11 19:50:35 -0400

    Initial Commit

---
+++ flaskBlog/__init__.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'd206478e70c5a17096b1934fb01057a6de7afc15b6c98055b1465a1879896d4d'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'warning'
+
+from flaskBlog import routes
